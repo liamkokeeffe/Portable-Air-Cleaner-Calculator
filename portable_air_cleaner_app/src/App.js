19102334e41252527897df93a5b954e448efabd8
Liam Sexton <liams4@uw.edu>
2021-02-08 12:41:47 -0800

    Add home, about, and additional resources tabs

--- portable_air_cleaner_app/src/App.js
+++ portable_air_cleaner_app/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import {Home} from './Home.js';
+import {About} from './About.js';
+import {AdditionalResources} from './AdditionalResources.js';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+      <p>Logo</p>
+      <h1 id='app-title'>Portable Air Cleaner Calculator</h1>
       </header>
+
+      <Router>
+        <div>
+            <div className='tabs'>
+                <Link to='/' className='tab'>Home</Link>
+                <Link to='/about' className='tab'>About</Link>
+                <Link to='/additional-resources' className='tab'>Additional Resources</Link>
+            </div>
+          <Switch>
+          <Route exact path='/'>
+              <Home />
+          </Route>
+          <Route exact path='/about'>
+              <About />
+          </Route>
+          <Route exact path='/additional-resources'>
+              <AdditionalResources />
+          </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
