276c19ed03c5fb9171178ae96ce6251d1d84c52f
liamkokeeffe <okeeffel@uw.edu>
2021-02-16 16:08:50 -0800

    Finished screen 2 & 3 of the calculator screens

--- portable_air_cleaner_app/src/components/Calculator.js
+++ portable_air_cleaner_app/src/components/Calculator.js
@@ -1,19 +1,35 @@
 import React, { Component, useState } from 'react';
-import "./Calculator.css"
+import {VentilationInput} from './VentilationInput';
+import "./Calculator.css";
+import {VentilationInfo} from "./VentilationInfo";
+import {CADR} from "./CADR"
 
 export function Calculator(props) {
     const [roomArea, setRoomArea] = useState(0);
     const [roomHeight, setRoomHeight] = useState(0);
     const [units, setUnits] = useState(0);
-    const [ventilationType, setVentilationType] = useState(0);
+    const [ventilationType, setVentilationType] = useState("Low");
     const [cadr, setCadr] = useState(0);
 
+    function updateVentilationType(type) {
+        setVentilationType(type);
+    }
 
+    let calculatorTitle = ""
+    if (props.title === "find") {
+        calculatorTitle = "Find Air Cleaner"
+    } else {
+        calculatorTitle = "Test Air Cleaner Efficiency"
+    }
 
     return (
-        <div>
-            <h2>{props.title}</h2>
-            <div id="calculatorBox"></div>
+        <div id="calculator-wrapper">
+            <h2 id="calculator-title">{calculatorTitle}</h2>
+            <div id="calculatorBox">
+                <VentilationInput updateVentilationType = {updateVentilationType} />
+                <VentilationInfo type = {ventilationType} />
+                {/* <CADR /> */}
+            </div>
         </div>
     )
 }
