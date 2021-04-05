66cf4c3b9c1b57dbf9be50fcf702dd43abd1faf8
liamkokeeffe <okeeffel@uw.edu>
2021-04-04 17:52:16 -0700

    fixed routing to work locally

--- portable_air_cleaner_app/src/components/Routes.js
+++ portable_air_cleaner_app/src/components/Routes.js
@@ -8,9 +8,9 @@ export function Routes() {
     return (
         <div>
             <Switch>
-                <Route exact path={process.env.PUBLIC_URL + 'Portable-Air-Cleaner-Calculator/'} component={Home} />
-                <Route exact path={process.env.PUBLIC_URL + 'Portable-Air-Cleaner-Calculator/about'} component={About} />
-                <Route exact path={process.env.PUBLIC_URL + 'Portable-Air-Cleaner-Calculator/additional-resources'} component={AdditionalResources} />
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/additional-resources' component={AdditionalResources} />
             </Switch>
         </div>
     )
