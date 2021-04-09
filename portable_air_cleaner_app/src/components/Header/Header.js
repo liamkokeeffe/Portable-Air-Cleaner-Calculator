9b7130c98d432cfe30ccc4733bac07b35a373151
Liam Sexton <liams4@uw.edu>
2021-04-08 22:57:39 -0700

    Update header styling

--- portable_air_cleaner_app/src/components/Header/Header.js
+++ portable_air_cleaner_app/src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import './Header.css'
 import logo from '../../images/departmentOfCommerceLogo.png';
-import coralLogo from '../../images/coral_logo.png';
+import {LanguageSelection} from './LanguageSelection.js';
 import { useTranslation } from 'react-i18next';
 
 export function Header() {
@@ -9,22 +9,19 @@ export function Header() {
     return (
         <div id="header-container">
             <header>
-                <div>
-                    <img className='logo' src={logo} alt='Washington State Department of Commerce Logo' width='550' height='250'/>
+                <img id='wa-dept-of-commerce-logo' src={logo} alt='Washington State Department of Commerce Logo' width='550' height="250" />
+                <div id="tabs-container">
+                    <div className='tab'>
+                        <a href="/" className="tab">{t('Tabs.Calculator')}</a>
+                        <span className="tab-divider">|</span>
+                        <a href="/about" className="tab">{t('Tabs.About')}</a>
+                        <span className="tab-divider">|</span>
+                        <a href="/additional-resources" className="tab">{t('Tabs.Resources')}</a>
+                    </div>
                 </div>
+                <LanguageSelection />
             </header>
-            <div id="tabs-container">
-                <div className='tab'>
-                    <a href="/" className="tab">{t('Tabs.Calculator')}</a>
-                    <span className="tab-divider">|</span>
-                    <a href="/about" className="tab">{t('Tabs.About')}</a>
-                    <span className="tab-divider">|</span>
-                    <a href="/additional-resources" className="tab">{t('Tabs.Resources')}</a>
-                </div>
-            </div>
-            <div id="team-logo-container">
-                <img src={coralLogo} alt="Team Logo" id="team-logo"/>
-            </div>
+            
         </div>
     )
 }
