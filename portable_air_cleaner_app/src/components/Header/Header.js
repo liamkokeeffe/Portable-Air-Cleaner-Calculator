4a0883fc6d0a399ea5c34c8c10e216fde0ecf5e7
Liam Sexton <liams4@uw.edu>
2021-04-13 21:46:05 -0700

    Add style and responsive design changes to header and landing page

--- portable_air_cleaner_app/src/components/Header/Header.js
+++ portable_air_cleaner_app/src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import './Header.css'
+import './Header.css';
 import logo from '../../images/departmentOfCommerceLogo.png';
 import {LanguageSelection} from './LanguageSelection.js';
 import { useTranslation } from 'react-i18next';
@@ -6,19 +6,32 @@ import { useTranslation } from 'react-i18next';
 export function Header() {
     const {t} = useTranslation();
 
+    function expandHamburgerMenu() {
+        if (document.querySelector('#tabs-container').style.display === '') {
+            document.querySelector('#tabs-container').style.display = 'flex';
+        } else {
+            document.querySelector('#tabs-container').style.display = '';
+        }
+    }
+
     return (
         <div id="header-container">
             <header>
-                <img id='wa-dept-of-commerce-logo' src={logo} alt='Washington State Department of Commerce Logo' width='550' height="250" />
-                <div id="tabs-container">
-                    <div className='tab'>
-                        <a href="/" className="tab">{t('Tabs.Calculator')}</a>
+                <img id='wa-dept-of-commerce-logo' src={logo} alt='Washington State Department of Commerce Logo' />
+                <nav>
+                    <div id="tabs-container">
+                        <a href="/" className="tab">{t('Tabs.Home')}</a>
                         <span className="tab-divider">|</span>
                         <a href="/about" className="tab">{t('Tabs.About')}</a>
                         <span className="tab-divider">|</span>
-                        <a href="/additional-resources" className="tab">{t('Tabs.Resources')}</a>
+                        <a id="resources-tab" href="/additional-resources" className="tab">{t('Tabs.Resources')} </a>
                     </div>
-                </div>
+                    <div id='hamburger-menu' onClick={() => expandHamburgerMenu()}>
+                        <div className="hamburger-menu-line"></div>
+                        <div className="hamburger-menu-line"></div>
+                        <div className="hamburger-menu-line"></div>
+                    </div>
+                </nav>
                 <LanguageSelection />
             </header>
         </div>
